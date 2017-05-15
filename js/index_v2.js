74f7fd077c6fb9e15d89f4516daeecf25b36f1ca
AlanNP <alanjamil14@gmail.com>
2017-05-15 13:57:10 -0500

    pruebas login

--- js/index_v2.js
+++ js/index_v2.js
@@ -1,42 +1,5 @@
 document.addEventListener("deviceready", Notificaciones, false);
 
 function Notificaciones(){
-    var push = PushNotification.init({
-        android: {
-            senderID: "1012969710793"
-        },
-        browser: {
-            pushServiceURL: 'http://push.api.phonegap.com/v1/push'
-        },
-        ios: {
-            alert: "true",
-            badge: "true",
-            sound: "true"
-        }
-    });
 
-    push.on('registration', function(data) {
-        var REGID=data.registrationId;
-            sessionStorage.setItem("REGID",REGID);
-            $("#btn_session").removeClass('ui-disabled');
-            $("#btn_session").html('Iniciar Sesión');
-    });
-
-    push.on('notification', function(data) {
-        navigator.notification.alert(
-            data.message,         // message
-            null,                 // callback
-            data.title,           // title
-            'Ok'                  // buttonName
-        );
-        push.finish(function() {
-        // Do something
-        });
-    });
-
-    push.on('error', function(e) {
-        if (e.code != 7) {
-            modal_errores(e.message);
-        }
-    });
 }
