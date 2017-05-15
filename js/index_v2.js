a6762e6dc38418358ff82bf86e96a1d9aab9da08
AlanNP <alanjamil14@gmail.com>
2017-05-15 12:03:38 -0500

    pruebas login

--- js/index_v2.js
+++ js/index_v2.js
@@ -1,6 +1,5 @@
-document.addEventListener("deviceready", onDeviceReady, false);
 
-function onDeviceReady(){
+$(function (){
     var push = PushNotification.init({
         android: {
             senderID: "1012969710793"
@@ -15,5 +14,28 @@ function onDeviceReady(){
         }
     });
 
+    push.on('registration', function(data) {
+        var REGID=data.registrationId;
+            sessionStorage.setItem("REGID",REGID);
+            $("#btn_session").removeClass('ui-disabled');
+            $("#btn_session").html('Iniciar Sesión');
+    });
 
-}
+    push.on('notification', function(data) {
+        navigator.notification.alert(
+            data.message,         // message
+            null,                 // callback
+            data.title,           // title
+            'Ok'                  // buttonName
+        );
+        push.finish(function() {
+        // Do something
+        });
+    });
+
+    push.on('error', function(e) {
+        if (e.code != 7) {
+            modal_errores(e.message);
+        }
+    });
+});
