139c05242ad50cb901ab9b6a921cd1e368175bd2
AlanNP <alanjamil14@gmail.com>
2017-05-15 11:45:04 -0500

    pruebas login

--- js/index_v2.js
+++ js/index_v2.js
@@ -1,5 +1,19 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady(){
+    var push = PushNotification.init({
+        android: {
+            senderID: "1012969710793"
+        },
+        browser: {
+            pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+        },
+        ios: {
+            alert: "true",
+            badge: "true",
+            sound: "true"
+        }
+    });
+
 
 }
