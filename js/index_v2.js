d5449e1ca2601d7d5912791cccb4073c669d7178
AlanNP <alanjamil14@gmail.com>
2017-05-15 13:31:58 -0500

    pruebas login

--- js/index_v2.js
+++ js/index_v2.js
@@ -1,17 +1,42 @@
+document.addEventListener("deviceready", Notificaciones, false);
 
-document.addEventListener("deviceready",onDeviceReady,false);
-function onDeviceReady(){
-   var push = PushNotification.init({ "android": {"senderID": "1012969710793"}});
-   push.on('registration', function(data) {
-       var REGID=data.registrationId;
-       sessionStorage.setItem("REGID",REGID);
-   });
+function Notificaciones(){
+    var push = PushNotification.init({
+        android: {
+            senderID: "1012969710793"
+        },
+        browser: {
+            pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+        },
+        ios: {
+            alert: "true",
+            badge: "true",
+            sound: "true"
+        }
+    });
 
-   push.on('notification', function(data) {
-       alert(data.title+" Message: " +data.message);
-   });
+    push.on('registration', function(data) {
+        var REGID=data.registrationId;
+            sessionStorage.setItem("REGID",REGID);
+            $("#btn_session").removeClass('ui-disabled');
+            $("#btn_session").html('Iniciar Sesión');
+    });
 
-   push.on('error', function(e) {
-       alert(e);
-   });
+    push.on('notification', function(data) {
+        navigator.notification.alert(
+            data.message,         // message
+            null,                 // callback
+            data.title,           // title
+            'Ok'                  // buttonName
+        );
+        push.finish(function() {
+        // Do something
+        });
+    });
+
+    push.on('error', function(e) {
+        if (e.code != 7) {
+            modal_errores(e.message);
+        }
+    });
 }
